9f77e9a224fafdb723d34cc646309987da3251a6
Arshita <122286846+Arshita-k@users.noreply.github.com>
2024-03-10 20:00:02 +0530

    Insertion_sort.cpp

---
+++ Insertion_sort.cpp
@@ -0,0 +1,36 @@
+# include <bits/stdc++.h>
+using namespace std;
+//insert elements
+//first element is already sorted becuase its a single element
+//23 is sorted
+//23 1 -> 1 23 swap
+//1 23 10 -> 1 10 23 swap
+//1 10 23 5 -> 1 5 10 23 swap
+
+//Time complexity is O(n^2)
+
+int Insertion_Sort(int arr[],int size)
+{
+    int i,key,j;
+    for(i=1;i<size;i++)
+    {
+        key=arr[i];
+        j=i-1;
+    }
+    //move elements of arr[0,i-1] that are greater than key 
+    //to one position ahead of their current position
+
+    while(j>=0 && arr[j]>key)
+    {
+        arr[j+1]=arr[j];
+        j=j-1;
+    }
+    arr[j+1]=key;
+}
+int main()
+{
+    int arr[]={23,1,10,5,2};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    Insertion_Sort(arr,size);
+    return 0;
+}
