5d40e61cddd6d07d70802cf36ecb6fd00f722d5a
Arshita <122286846+Arshita-k@users.noreply.github.com>
2024-06-05 15:49:11 +0530

    Update
    Reverse a linked list using iterative and recursive approach 
    another approach can be using stack insert in stack and the creat a linked list by extracting elements from stack

---
+++ Linked_list/medium/reverse_singleLL.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int value)
+    {
+        data=value;
+        next=NULL;
+    }
+};
+//ITERATIVE APPROACH
+Node* reverse(Node*& head)
+{
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL)
+    {
+        Node* front = temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=front;
+    }
+    return prev;
+}
+void printLinkedList(Node*& head) 
+{
+    Node* temp = head;
+    while (temp != nullptr) 
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node* head = new Node(1);
+    Node* node2 = new Node(2);
+    head->next=node2;
+    Node* node3 = new Node(3);
+    node2->next=node3;
+    printLinkedList(head);
+    head=reverse(head);
+    printLinkedList(head);
+    return 0;
+    // Node* node4 = new Node(4);
+    // node3->next=node4;
+    // Node* node5 = new Node(5);
+    // node4->next=node5;
+}
+/*
+OUTPUT
+
+1 -> 2 -> 3 
+3 -> 2 -> 1 
+
+*/
