ed8d8d4902308b533a22c53868947a19205f7571
Arshita <122286846+Arshita-k@users.noreply.github.com>
2024-06-25 16:23:34 +0530

    Create Adjacency_Matrix.cpp

---
+++ Graphs/Adjacency_Matrix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cout<<"Enter number of Nodes/Vertices : ";
+    cin>>n;
+    cout<<"Enter number of Edges : ";
+    cin>>m;
+    //considering its 1 based indexing
+    int adj[n+1][n+1]={0};
+    //adjacency matrix
+    for(int i=0;i<m;i++)
+    {
+        int a,b;
+        cin<<a<<b;
+        adj[a][b]=1; //its a undirected graph
+        adj[b][a]=1;      //if it was a directed graph then order matters  
+    }
+    return 0;
+}
